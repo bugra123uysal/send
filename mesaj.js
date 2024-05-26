1b363f15ded1adc4a207a89716153fd2ed266cd7
buğra uysal <uysalbugra134@gmail.com>
2024-05-26 23:57:28 +0300

    github a eklendi

---
+++ mesaj.js
@@ -0,0 +1,27 @@
+
+
+
+
+
+/* kayıt olma bölümü */
+/* kullanıcı ismini yazdırmak */
+var kadı=localStorage.getItem("namee");
+document.getElementById("nad").innerHTML=kadı
+
+
+
+/* mesaj gönderme  */
+function gndr(){
+  var ismim=document.getElementById("nad").value
+
+   var mesajım= document.getElementById("yazz").value;
+  var textt= document.getElementById("görm").innerHTML=ismim+":" +" "+mesajım
+  
+
+
+}
+
+
+
+
+
