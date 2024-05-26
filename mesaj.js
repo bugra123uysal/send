4083120168d6b476595640f680a66985933d01e7
buğra uysal <uysalbugra134@gmail.com>
2024-05-27 01:53:53 +0300

    mesajların göüktüğü yeri dizenleme

--- mesaj.js
+++ mesaj.js
@@ -6,21 +6,28 @@
 /* kayıt olma bölümü */
 /* kullanıcı ismini yazdırmak */
 var kadı=localStorage.getItem("namee");
-document.getElementById("nad").innerHTML=kadı
+var ismim=document.getElementById("nad").innerHTML=kadı
+var gg=document.getElementById("görm");
 
 
 
 /* mesaj gönderme  */
 function gndr(){
-  var ismim=document.getElementById("nad").value
 
-   var mesajım= document.getElementById("yazz").value;
-  var textt= document.getElementById("görm").innerHTML=ismim+":" +" "+mesajım
+  var kmes=document.createElement("p");
+  kmes.id="nad"
+  var bbşlk=document.createElement("br");
+
+  var mesajım= document.getElementById("yazz").value;
+
+  kmes.textContent=ismim+":"+" "+mesajım;
+ 
+  gg.appendChild(kmes)
+  gg.appendChild(bbşlk)
+gg.scrollTop=gg.scrollHeight
+
   
-
-
 }
-
 
 
 
